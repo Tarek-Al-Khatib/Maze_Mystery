7977d96659e2b9362b1cba249db3378e3f2ce5d7
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-10-29 14:25:41 +0200

    Add: set action animation to the player when the user presses up, down, left, or right

--- level-3.js
+++ level-3.js
@@ -23,12 +23,46 @@ class level3 extends Phaser.Scene {
         //player section
         player = this.physics.add.sprite(150, 110, "tard_left"); // add a player as a physical object on map
         
-
-
+        //player movement animation 
+        this.anims.create({ key: "left", frames: this.anims.generateFrameNumbers("tard_left", { start: 0, end: 22, }), frameRate: 20, repeat: -1, });
+        this.anims.create({ key: "right",frames: this.anims.generateFrameNumbers("tard_right", {start: 0,end: 22,}),frameRate: 22,repeat: -1,});
+        this.anims.create({ key: 'up', frames: this.anims.generateFrameNumbers('tard_top', { start: 0, end: 22 }), frameRate: 22, repeat: -1 });
+        this.anims.create({ key: 'down', frames: this.anims.generateFrameNumbers('tard_down', { start: 0, end: 22 }), frameRate: 22, repeat: -1 });
+        this.anims.create({ key: 'turn', frames: [{ key: 'tard_left', frame: 0 }], frameRate: 20 });
         
+        //to detect user input
+        cursors = this.input.keyboard.createCursorKeys();
+
+
     }
 
     update() {
+
+        //set action animation to player when user press up,down, left or wright
+        const speed = 200;
+        let moveX = 0;
+        let moveY = 0;
+
+        if (cursors.left.isDown) {
+            moveX = -speed;
+            player.anims.play("left", true);
+        } else if (cursors.right.isDown) {
+            moveX = speed;
+            player.anims.play("right", true);
+        }
+
+        if (cursors.up.isDown) {
+            moveY = -speed;
+            player.anims.play("up", true);
+        } else if (cursors.down.isDown) {
+            moveY = speed;
+            player.anims.play("down", true);
+        }
+
+        if (moveX === 0 && moveY === 0) {
+            player.setVelocity(0, 0);
+            player.anims.play("turn", true);
+        }
 
     }
 }
